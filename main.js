ed257b15551f80a215748f0607114215c8fbfced
Josearmandoforerogonzalez <joseforero0310@gmail.com>
2021-07-14 21:17:51 -0500

    operadores aritmeticos 14/7/21

---
+++ main.js
@@ -0,0 +1,74 @@
+//Pedir 2 datos por pantalla y almacenarlo en dos variables 
+//diferentes:
+//Variable 1(num1)  Variable 2 (num2)
+//Realizar todas las Operacion arigameticas vistas en la clase
+//Mostra en la consola todos los posibles resultados
+//Extra: Por medio de una venta emergente(alert) Dar la maxima
+//informacion del desarrallodar del aplicativo en cuestion
+
+
+alert("jose forero gonzalez, creador de este programa llamado resultados, el cual calcula el resultado de dos variables, segun el dato que se introdusca")
+
+let num1 = parseInt(prompt("ingresa un valor para realizar una adición"))
+let num2 = parseInt(prompt("ingrsa el segundo valor"))
+
+alert("oprime f12 para ver el resultado")
+console.log( num1 + num2)
+
+
+
+// let num1 = parseInt(prompt("ingresa un valor para realizar una multiplicacion"))
+// let num2 = parseInt(prompt("ingrsa el segundo valor"))
+
+
+// alert("oprime f12 para ver el resultado")
+// console.log(num1 * num2)
+
+// let num1 = parseInt(prompt("ingresa un valor para realizar una division"))
+// let num2 = parseInt(prompt("ingrsa el segundo valor"))
+
+
+// alert("oprime f12 para ver el resultado")
+// console.log(num1 / num2)
+
+// let num1 = parseInt(prompt("ingresa un valor para realizar un resto o modulo"))
+// let num2 = parseInt(prompt("ingrsa el segundo valor"))
+
+
+// alert("oprime f12 para ver el resultado")
+// console.log(num1 % num2)
+
+// let num1 = parseInt(prompt("ingresa un valor para realizar una sustraccion"))
+// let num2 = parseInt(prompt("ingrsa el segundo valor"))
+
+
+// alert("oprime f12 para ver el resultado")
+// console.log(num1 - num2)
+
+
+// pos-incremento 
+// debugger
+// let num = 0;
+// num++;
+// num++;
+// num++;
+// num++;
+// num++;
+// console.log(num);
+
+
+// pre-incremento 
+// let num = 5;
+
+// alert(++num);
+
+
+// pos-decremento 
+// let num = 10;
+// alert(-num);
+
+// modulo = 1
+// multiplicacion = 2
+// division =  3
+// resta = 4
+// suma = 5
